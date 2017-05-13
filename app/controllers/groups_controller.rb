8200351b9f8461e43dedc1bd21ade71dc7ac1d7e
dingzhong <dingzhong55555@gmail.com>
2017-05-13 11:53:45 +0800

    create a group , join it

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -15,6 +15,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -54,6 +55,36 @@ class GroupsController < ApplicationController
 
     @group.delete
     redirect_to groups_path, alert: "Delete success"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if
+      !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你不是成员！"
+    end
+
+    redirect_to group_path(@group)
+
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if
+      current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版!"
+    else
+      flash[:warning] = "你不是成员！"
+    end
+
+    redirect_to group_path(@group)
+
   end
 
   private
